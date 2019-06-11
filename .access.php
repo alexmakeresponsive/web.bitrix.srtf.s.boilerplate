6a2f4f6d0e1900c76c04645f2828e00570dc0abb
Alexandr Gorchakov <alexmakeresponsive@gmail.com>
2019-06-11 15:49:16 +0600

    .access.php: update $PERM

--- .access.php
+++ .access.php
@@ -1,8 +1,5 @@
 <?
-$PERM["personal"]["8"]="W";
-$PERM["catalog"]["8"]="W";
-$PERM["news"]["8"]="W";
-$PERM["about"]["8"]="W";
+$PERM["local/templates/eshop_bootstrap_black/include/page"]["8"]="W";
 $PERM["index.php"]["8"]="W";
 $PERM["/"]["*"]="R";
 ?>
